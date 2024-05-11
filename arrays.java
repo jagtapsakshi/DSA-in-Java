2d0d875c9c5a1890315fbaadb0323f463301da11
jagtapsakshi <jagtapsakshi12@gmail.com>
2024-05-11 15:20:47 +0530

    Arrays basic code added.

---
+++ arrays.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class arrays {
+
+    public static void main(String args[]){
+
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("Enter size of arr:");
+        int size = sc.nextInt();
+
+        System.out.println("ENter number to search:");
+        int x = sc.nextInt();
+        
+        int array[] = new int[size];
+        //input
+        System.out.println("Enter arr elements: ");
+        for(int i=0; i<size; i++){
+            array[i] = sc.nextInt();
+        }
+        //output
+        for(int i=0; i<size; i++){
+            if(array[i] == x){System.out.println(array[i]+" "+i);    
+        }
+                    
+        }
+
+
+
+    }
+    
+}
