3d06433d614ada18c8e73378852da100a035b959
jagtapsakshi <jagtapsakshi12@gmail.com>
2024-05-11 15:42:38 +0530

    Finding index of largest number in sorted array

---
+++ largestindex.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class largestindex{
+
+    public static int largest(int array[]){
+
+        int n = array.length;
+
+        int lidx =0;
+
+        for(int i=0; i<n; i++){
+            if(array[i]>array[lidx]){
+                lidx = i;                 
+            }       
+        }
+        return lidx;
+
+    }
+    
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of arr:");
+        int size = sc.nextInt();
+
+        int array[] = new int[size];
+        //input
+        System.out.println("Enter arr elements: ");
+        for(int i=0; i<size; i++){
+            array[i] = sc.nextInt();
+        }
+        //output
+        System.out.println("The largest element is present at index "+ largest(array));
+
+    }
+}
+
